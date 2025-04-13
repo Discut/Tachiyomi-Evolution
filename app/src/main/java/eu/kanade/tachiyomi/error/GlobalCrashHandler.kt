8f4e7020911cf7dc0130acdf3787c241beff1ac8
Discut <siiiro@outlook.com>
2025-04-13 18:53:43 +0800

    feat：支持全局崩溃捕获

--- app/src/main/java/eu/kanade/tachiyomi/error/GlobalCrashHandler.kt
+++ app/src/main/java/eu/kanade/tachiyomi/error/GlobalCrashHandler.kt
@@ -1,14 +1,21 @@
 package eu.kanade.tachiyomi.error
 
+import android.app.Activity
+import android.app.ActivityManager
+import android.app.ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND
+import android.app.Application
 import android.content.Context
+import android.content.Context.ACTIVITY_SERVICE
+import eu.kanade.tachiyomi.App
 import eu.kanade.tachiyomi.util.system.toast
 import timber.log.Timber
+import java.lang.ref.WeakReference
 
 /**
  * 全局错误捕获
  */
 class GlobalCrashHandler(
-    private val context: Context,
+    private val context: Application,
     private val defaultHandler: Thread.UncaughtExceptionHandler? =
         Thread.getDefaultUncaughtExceptionHandler(),
 ) : Thread.UncaughtExceptionHandler {
@@ -22,9 +29,8 @@ class GlobalCrashHandler(
                 if (throwable.isHandled) {
                     return
                 }
-                throwable.messageResId?.let {
-                    context.toast(it)
-                } ?: context.toast(throwable.message)
+
+                showErrorToast(throwable)
 
                 if (throwable.isNeedLog) {
                     Timber.w(throwable)
@@ -37,4 +43,32 @@ class GlobalCrashHandler(
             }
         }
     }
+
+    private fun showErrorToast(throwable: TipsException) {
+        (context as? App)?.apply { // 自定义 Application 类
+            if (isAppInForeground(context)) {
+                ActivityHolder.currentActivity?.get()?.apply {
+                    runOnUiThread {
+                        throwable.messageResId?.let {
+                            context.toast(it)
+                        } ?: context.toast(throwable.message)
+                    }
+                }
+            } else {
+                // 发送系统通知或记录到文件
+            }
+        }
+    }
+
+    private fun isAppInForeground(context: Context): Boolean {
+        val activityManager = context.getSystemService(ACTIVITY_SERVICE) as ActivityManager
+        val runningProcesses = activityManager.runningAppProcesses ?: return false
+        return runningProcesses.any {
+            it.processName == context.packageName && it.importance == IMPORTANCE_FOREGROUND
+        }
+    }
+}
+
+object ActivityHolder {
+    @Volatile var currentActivity: WeakReference<Activity>? = null
 }
